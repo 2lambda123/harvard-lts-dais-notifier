d167ac3f180e16e1b4c2012804884f79e0d606a0
Valdeva Crema <valdeva_crema@harvard.edu>
2023-04-16 21:22:31 -0400

    Notifier Microservice (#1)
    * initial commit
    * updating messaging
    * PR template
    * Readme
    * added healthcheck
    * Updated jenkins and port
    * Added git folder to image for healthcheck.  Updated git healtcheck error message
    * Updated docker version
    
    Resolves: https://jira.huit.harvard.edu/browse/LTSEPADD-94

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # dais-notifier
+
+A microservice that reads messages from a queue and sends out an email
+
+
+## Local setup
+    
+1. Make a copy of the env-template.txt to .env and modify the user and password variables.
+
+2. Start the container
+    
+```
+docker-compose -f docker-compose-local.yml up -d --build --force-recreate
+```
+
+3. Local Healthcheck: https://localhost:10587/healthcheck
+
+## Testing
+Note, testing uses its own queues so they will not interfere with the queues used by the actual program.
+
+1. Start the container up as described in the <b>Local Setup</b> instructions.
+
+2. Exec into the container:
+
+```
+docker exec -it notifier bash
+```
+
+3. Run the tests
+
+```
+pytest
+```
+
+
+
---
+++ app/notifier/dais_notifier.py
@@ -0,0 +1,64 @@
+import os, os.path, logging
+import smtplib
+from email.message import EmailMessage
+from tenacity import retry, stop_after_attempt
+
+logger = logging.getLogger('dais-notifier')
+class SmtpMailingService():
+    __SMTP_SEND_MESSAGE_MAX_RETRIES = 2
+    
+
+
+    def send_email(self, subject, body, recipients = None):
+        logger.info("Creating email message with subject {} and body {}...".format(subject, body))
+        email_sender = os.getenv("EMAIL_FROM")
+        if recipients is None:
+            email_destination = os.getenv("EMAIL_DEFAULT_RECIPIENT")
+        else:
+            email_destination = recipients
+        
+        message = self.__create_message(subject, body, email_destination)
+        logger.info(
+            "Sending email with subject {}, via SMTP...".format(subject)
+        )
+        
+        return self.__send_smtp_message(message)
+
+    def __create_message(self, email_subject, email_body, email_destination):
+        logger.info("Creating email message with subject {} and body {}...".format(email_subject, email_body))
+
+        email_sender = os.getenv("EMAIL_FROM")
+        logger.info(
+            "Sender is {}, and destination is {}, for email with subject {}".format(
+                email_sender,
+                email_destination,
+                email_subject
+            )
+        )
+
+        message = EmailMessage()
+        message.set_content(email_body)
+
+        message['Subject'] = email_subject
+        message['From'] = email_sender
+        message['To'] = email_destination
+
+        return message
+
+    @retry(
+        stop=stop_after_attempt(__SMTP_SEND_MESSAGE_MAX_RETRIES)
+    )
+    def __send_smtp_message(self, message):
+        email_host = os.getenv("EMAIL_HOST")
+        email_port = os.getenv("EMAIL_PORT")
+
+        try:
+            smtp = smtplib.SMTP(email_host, email_port)
+            smtp.send_message(message)
+            smtp.quit()
+        except smtplib.SMTPException as e:
+            logger.error(str(e))
+            raise e
+
+        logger.info("Email sent")
+        return True
